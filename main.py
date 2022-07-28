c6c4d995326760015c582b1927df9fa1f12fb499
BotrRobotgnom <user84443@gmail.com>
2022-07-28 23:42:17 +0300

    Revert "PEP-8 refactored and some typo fixes"
    This reverts commit 0b7cc4415c51517a79aaaca5c2e956b0ad17fa4d.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from discord.ext import commands
+from discord import default_permissions, option
 
 from config import settings
 
@@ -8,87 +9,103 @@ bot = commands.Bot(command_prefix=settings['PREFIX'], intents=discord.Intents.al
 
 bot.remove_command('help')
 
-cogs_list = []
+cogs_listt = []
 
-
-def auto_cogs_err_load(key: str = None):
-    if key is None:
-        for cogs in os.listdir("./cogs"):
-            if cogs == "__pycache__":
+def auto_cogserr_load(key: str = None):
+    if key == None:
+        for cogss in os.listdir("./cogs"):
+            if cogss == "__pycache__":
                 continue
-
-            if cogs.endswith(".py"):
-                print(f"cogs.{cogs[:-3]}")
-                bot.load_extension(f"cogs.{cogs[:-3]}")
-                cogs_list.append(cogs[:-3])
+            if cogss.endswith(".py"):
+                print(f"cogs.{cogss[:-3]}")
+                bot.load_extension(f"cogs.{cogss[:-3]}")
+                #lister
+                cogs_listt.append(cogss[:-3])
             else:
-                for cog in os.listdir(f"./cogs/{cogs}"):
+                for cog in os.listdir(f"./cogs/{cogss}"):
                     if cog == "__pycache__":
                         continue
                     if cog.endswith(".py"):
-                        print(f"cogs.{cogs}.{cog[:-3]}")
-                        bot.load_extension(f"cogs.{cogs}.{cog[:-3]}")
-                        cogs_list.append(cog[:-3])
-
+                        print(f"cogs.{cogss}.{cog[:-3]}")
+                        bot.load_extension(f"cogs.{cogss}.{cog[:-3]}")
+                        #lister
+                        cogs_listt.append(cog[:-3])
     else:
-        for cogs in os.listdir("./cogs"):
-            if cogs == "__pycache__":
+        for cogss in os.listdir("./cogs"):
+            if cogss == "__pycache__":
                 continue
-
-            if cogs.endswith(".py"):
-                if str(cogs[:-3]) == key:
-                    print(f"cogs.{cogs[:-3]}")
-                    bot.load_extension(f"cogs.{cogs[:-3]}")
+            if cogss.endswith(".py"):
+                if str(cogss[:-3]) == key:
+                    print(f"cogs.{cogss[:-3]}")
+                    bot.load_extension(f"cogs.{cogss[:-3]}")
             else:
-                for cog in os.listdir(f"./cogs/{cogs}"):
+                for cog in os.listdir(f"./cogs/{cogss}"):
                     if cog == "__pycache__":
                         continue
                     if cog.endswith(".py"):
                         if str(cog[:-3]) == key:
-                            print(f"cogs.{cogs}.{cog[:-3]}")
-                            bot.load_extension(f"cogs.{cogs}.{cog[:-3]}")
+                            print(f"cogs.{cogss}.{cog[:-3]}")
+                            bot.load_extension(f"cogs.{cogss}.{cog[:-3]}")
 
 
-def auto_cogs_err_unload(key: str = None):
-    if key is None:
-        for cogs in os.listdir("./cogs"):
-            if cogs == "__pycache__":
+def auto_cogserr_unload(key: str = None):
+    if key == None:
+        for cogss in os.listdir("./cogs"):
+            if cogss == "__pycache__":
                 continue
-            if cogs.endswith(".py"):
-                print(f"cogs.{cogs[:-3]}")
-                bot.unload_extension(f"cogs.{cogs[:-3]}")
+            if cogss.endswith(".py"):
+                print(f"cogs.{cogss[:-3]}")
+                bot.unload_extension(f"cogs.{cogss[:-3]}")
             else:
-                for cog in os.listdir(f"./cogs/{cogs}"):
+                for cog in os.listdir(f"./cogs/{cogss}"):
                     if cog == "__pycache__":
                         continue
                     if cog.endswith(".py"):
-                        print(f"cogs.{cogs}.{cog[:-3]}")
-                        bot.unload_extension(f"cogs.{cogs}.{cog[:-3]}")
+                        print(f"cogs.{cogss}.{cog[:-3]}")
+                        bot.unload_extension(f"cogs.{cogss}.{cog[:-3]}")
     else:
-        for cogs in os.listdir("./cogs"):
-            if cogs == "__pycache__":
+        for cogss in os.listdir("./cogs"):
+            if cogss == "__pycache__":
                 continue
-            if cogs.endswith(".py"):
-                if str(cogs[:-3]) == key:
-                    print(f"cogs.{cogs[:-3]}")
-                    bot.unload_extension(f"cogs.{cogs[:-3]}")
+            if cogss.endswith(".py"):
+                if str(cogss[:-3]) == key:
+                    print(f"cogs.{cogss[:-3]}")
+                    bot.unload_extension(f"cogs.{cogss[:-3]}")
             else:
-                for cog in os.listdir(f"./cogs/{cogs}"):
+                for cog in os.listdir(f"./cogs/{cogss}"):
                     if cog == "__pycache__":
                         continue
                     if cog.endswith(".py"):
                         if str(cog[:-3]) == key:
-                            print(f"cogs.{cogs}.{cog[:-3]}")
-                            bot.unload_extension(f"cogs.{cogs}.{cog[:-3]}")
+                            print(f"cogs.{cogss}.{cog[:-3]}")
+                            bot.unload_extension(f"cogs.{cogss}.{cog[:-3]}")
 
 
 @bot.event
 async def on_ready():
     print('Бот запустился')
 
+auto_cogserr_load()
+print(cogs_listt)
 
-auto_cogs_err_load()
-print(cogs_list)
-
+@bot.slash_command(guild_ids = [1001238238897373225],name ="cre", description="cogs")
+@default_permissions(administrator=True)
+@option("cog", description="cog", choices=cogs_listt)
+async def slash__cre(ctx,cog: str):
+    if ctx.user.id == (318805947084439552): 
+        if cog != None:
+            try:
+                auto_cogserr_unload(cog)
+            except:
+                print(f"Cog не отгружается {cog}")
+            try:
+                auto_cogserr_load(cog)
+            except:
+                print(f"Cog не загружается {cog}")
+            return await ctx.respond(f"Cog {cog} перезагружен", ephemeral=True)
+        else:
+            return await ctx.respond(f"Cog не указан", ephemeral=True)
+    else:       
+        return await ctx.respond("Ты кто?????", ephemeral=True)
 
 bot.run(settings['TOKEN'])
--- sqlfunc.py
+++ sqlfunc.py
@@ -2,48 +2,43 @@ import pymysql
 from config import sql
 import json
 
-
 def transform_sql_to_norm(rows):            
     l_row = []
     for k in rows.items():
         l_row.append(k[1])
     return l_row
 
-
-def sql_py(key, sql_quest):
+def sql_py(key,sql_quest):
     key_print = "sqlfunc.py: "
 
     try:
         connection = pymysql.connect(
-            host=sql['host'],
-            port=3306,
-            user=sql['user'],
-            password=sql['password'],
-            database=sql['db_name'],
+            host= sql['host'],
+            port= 3306,
+            user= sql['user'],
+            password= sql['password'],
+            database= sql['db_name'],
             cursorclass=pymysql.cursors.DictCursor)
         try:
             with connection.cursor() as cursor:
                 if key == "get":
-                    cursor.execute("SELECT * FROM smashup WHERE server_id = %s", (sql_quest[0]))
+                    cursor.execute("SELECT * FROM smashup WHERE server_id = %s",(sql_quest[0]))
                     rows = cursor.fetchone()
-                    if rows is None:
+                    if rows == None:
                         sample_list = []
                         list_to_json_array = json.dumps(sample_list)
-                        cursor.execute("INSERT INTO smashup (server_id, back_play, now_play, next_play) "
-                                       "VALUES (%s, %s, %s, %s)",
-                                       (str(sql_quest[0]), list_to_json_array, list_to_json_array, list_to_json_array))
+                        insert_query = "INSERT INTO smashup (server_id, back_play, now_play, next_play) VALUES (%s,%s,%s,%s);"
+                        cursor.execute(insert_query, (str(sql_quest[0]),list_to_json_array,list_to_json_array,list_to_json_array))
                         connection.commit()
 
-                        cursor.execute("SELECT * FROM smashup WHERE server_id = %s", (sql_quest[0]))
+                        cursor.execute("SELECT * FROM smashup WHERE server_id=%s",(sql_quest[0]))
                         rows = cursor.fetchone()
-                    return rows
+                    return (rows)
                 elif key == "update":
-                    cursor.execute("UPDATE smashup SET back_play = %s, now_play = %s, next_play = %s "
-                                   "WHERE server_id = %s", (sql_quest[1], sql_quest[2], sql_quest[3], sql_quest[0]))
+                    cursor.execute("UPDATE smashup SET back_play = %s, now_play = %s, next_play = %s WHERE server_id = %s;",(sql_quest[1],sql_quest[2],sql_quest[3],sql_quest[0]))
                     connection.commit()
                 elif key == "update_next":
-                    cursor.execute("UPDATE smashup SET next_play = %s "
-                                   "WHERE server_id = %s", (sql_quest[1], sql_quest[0]))
+                    cursor.execute("UPDATE smashup SET next_play = %s WHERE server_id = %s;",(sql_quest[1],sql_quest[0]))
                     connection.commit()
         finally:
             connection.close()
